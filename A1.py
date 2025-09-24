c740e75eca23316c015d34e88d36a8c38d5e8f60
Madalina C <costovim@tcd.ie>
2025-09-25 00:11:09 +0100

    Completed a)i) - Visualise the data on a 2D plot

---
+++ A1.py
@@ -0,0 +1,48 @@
+import sklearn as sk
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Data set:
+# id:24--48--24 
+
+
+# import data (week2.csv, given in assignment sheet)
+df = pd.read_csv("week2.csv")
+print(df.head())
+X1 = df.iloc[:,0] # exctracting all rows from column 0
+X2 = df.iloc[:,1] # extracting all rows from column 1
+X = np.column_stack((X1, X2))
+Y = df.iloc[:,2] # extracting all rows from column 2, want to plot this differenciating between +/-1
+
+# a) i) Plot data & distinguish +/- 1
+
+# set up arrays to store coordinates for X1 and X2 depending on the Y value (+/-1)
+x1Pos = []
+x1Neg = []
+x2Pos = []
+x2Neg = []
+
+# index
+idx = 0
+
+# need to get contents of cell using .iloc, see w2schools for more info
+for i in range(len(Y)):
+    if(Y.iloc[idx] == 1):
+        # if value at position idx is 1
+        x1Pos.insert(idx, X1.iloc[idx]) # add x1 coordinate to list of x1 coordinates where Y = 1
+        x2Pos.insert(idx, X2.iloc[idx]) # add x2 coordinate to list of x2 coordinates where Y = 1
+        idx += 1 # increment index
+    else:
+        x1Neg.insert(idx, X1.iloc[idx]) # add x1 coordinate to list of x1 coordinates where Y = -1
+        x2Neg.insert(idx, X2.iloc[idx]) # add x2 coordinate to list of x2 coordinates where Y = -1
+        idx += 1 # increment index
+
+
+plt.scatter(x1Pos, x2Pos, c = "red", marker = "+", label = "+1") # plot rows where Y = 1 on a scatter plot
+plt.scatter(x1Neg, x2Neg, c = "blue", marker = "_", label = "-1") # plot rows where Y = -1 on a scatter plot
+plt.title("Visualisation of +/-1") # plot title
+plt.xlabel("Column 0 data") # X axis label
+plt.ylabel("Column 1 data") # Y axis label
+plt.legend(loc = "upper right") # adding legend to plot and forcing it to top-right corner
+plt.show() # show plot
